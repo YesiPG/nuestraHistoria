1cf455903b7e41b16c7efc52e7b266dcccee719a
Yesica Perlaza <perlazayesik@gmail.com>
2023-07-04 15:43:57 -0500

    nav completo

---
+++ js/nav.js
@@ -0,0 +1,20 @@
+(function(){
+    "use strict";
+
+    const d = document;
+    const contNav = d.querySelector(".hNav");
+    const btnClose = d.querySelector(".hNavClose")
+    
+    d.addEventListener("click",(e)=>{
+        if(e.target.matches(".hMenu")){
+            contNav.classList.add("hNavShow");
+            btnClose.classList.add("hNavCloseShow");
+        }
+
+        if(e.target.matches(".hNavClose")){
+            
+            contNav.classList.remove("hNavShow");
+            btnClose.classList.remove("hNavCloseShow");
+        }
+    })
+})();
