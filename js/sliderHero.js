e064b77304c42028bb75a172314a70676460efb8
Yesica Perlaza <perlazayesik@gmail.com>
2023-07-04 17:48:44 -0500

    hero completado

---
+++ js/sliderHero.js
@@ -0,0 +1,24 @@
+(function () {
+  "use strict";
+
+  const d = document;
+  const carrusel =d.querySelector(".heroCarrusel");
+
+  let intervalo = null;
+  let step = 1;
+  let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
+
+  const empezar = ()=>{
+    intervalo = setInterval(()=>{
+        carrusel.scrollLeft = carrusel.scrollLeft + step;
+        if(carrusel.scrollLeft === maxScrollLeft){
+            step = step * -1;
+        } else if(carrusel.scrollLeft === 0){
+            step = step * -1;
+        }
+    },10);
+  }
+
+  empezar();
+
+})();
